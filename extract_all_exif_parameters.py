f7fc7568652a99eb938a2fa51715fabf41af0772
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2022-09-27 15:36:37 +0200

    ...

--- extract_all_exif_parameters.py
+++ extract_all_exif_parameters.py
@@ -1,7 +1,24 @@
 from exiftool import ExifToolHelper
 with ExifToolHelper() as et:
     filename = "input/DJI_0177.JPG"
-    for d in et.get_metadata(filename):
-        for k, v in d.items():
-            print(f"Dict: {k} = {v}")
+    tags = et.get_metadata(filename)
 
+    keys = ["XMP:GimbalRollDegree",
+            "XMP:GimbalPitchDegree", 
+            "XMP:GimbalYawDegree",
+            "XMP:CalibratedFocalLength", 
+            "XMP:CalibratedOpticalCenterX", 
+            "XMP:CalibratedOpticalCenterY",
+            "Composite:GPSLatitude",
+            "Composite:GPSLongitude",
+            "XMP:AbsoluteAltitude",
+            "XMP:RelativeAltitude",
+            "EXIF:ExifImageWidth",
+            "EXIF:ExifImageHeight"]
+
+
+    for key in keys:
+        try:
+            print(f"{ key } = { tags[0][key] }")
+        except:
+            pass
